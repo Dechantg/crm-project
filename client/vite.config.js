34eb45e29477adec2e6b3923f557869a1827cd13
Gregory Dechant <dechantg@gmail.com>
2024-03-10 11:10:51 -0700

    pushing fresh gitignore

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,28 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
+const apiBaseUrl = 'http://localhost:8089';
+
+
 export default defineConfig({
-    // depending on your application, base can also be "/"
     base: '',
     plugins: [react(), viteTsconfigPaths()],
-    define: {
-      // here is the main update
-      global: 'globalThis',
-    },
     server: {
+      port: 5174,
       host: '0.0.0.0',
-      port: 3000, 
-
       proxy: {
         '/api': {
-          target: "http://192.168.50.20:3003",
+          target: apiBaseUrl,
           changeOrigin: true,
           secure: false,
-          // We can even re-write the request
           rewrite: path => path.replace('/api', ''),
         }
       }    
-        // this ensures that the browser opens upon server start
-        // this sets a default port to 3000  
+    
     },
 })
